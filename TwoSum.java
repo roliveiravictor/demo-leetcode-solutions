bc2ffdbaab0d8ecf15bcba056973532578314e7c
xb204417 <roliveira.victor@gmail.com>
2018-09-07 23:48:17 -0300

    Added twoSum solution

---
+++ TwoSum.java
@@ -0,0 +1,48 @@
+/**
+*
+* Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+*
+* You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*
+* Example:
+*
+* Given nums = [2, 7, 11, 15], target = 9,
+*
+* Because nums[0] + nums[1] = 2 + 7 = 9,
+* return [0, 1].
+*
+**/
+
+@MineSolution
+public int[] twoSum(int[] nums, int target) {
+    for(int firstRunner = 0; firstRunner < nums.length; firstRunner++) {
+        for(int secondRunner = 0; secondRunner < nums.length; secondRunner++) {
+            if(firstRunner == secondRunner)
+                continue;
+
+            final int firstTargetBuilder = nums[firstRunner];
+            final int secondTargetBuilder = nums[secondRunner];
+            final int mTarget = firstTargetBuilder + secondTargetBuilder;
+            if(mTarget == target){
+                final int[] targetPosition = {firstRunner, secondRunner};
+                return targetPosition;
+            }
+        }
+    }
+    final int[] notFoundPosition = {0, 0};
+    return notFoundPosition;
+}
+
+
+@TopLeetCodeSolution
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
